890798998bf67d2e99ee9b889fdc22aa02615157
amberlb <amber.beckley@duke.edu>
2016-02-03 09:57:59 -0500

    Initial commit of letters data from ProjectTemplate tutorial

---
+++ src/generate_plots.R
@@ -0,0 +1,5 @@
+plot1 <- ggplot(first.letter.counts, aes(x = V1)) + geom_density()
+ggsave(file.path('graphs', 'plot1.pdf'))
+
+plot2 <- ggplot(second.letter.counts, aes(x = V1)) + geom_density()
+ggsave(file.path('graphs', 'plot2.pdf'))
